186cbdf2a70b2e87a63bb45be4eef9301d61a3de
Andrea Giuseppe Cavallaro <adenor88@gmail.com>
2026-03-28 22:43:58 +0100

    Refactor migration files and update .gitignore; remove unused Alembic configurations and scripts

--- seed/seed.py
+++ seed/seed.py
@@ -3,11 +3,11 @@
 import os
 import sys
 
+from src.app import create_app
+from src.populate import populate_books
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
-from src.app import create_app  # noqa: E402
-from src.populate import populate_books  # noqa: E402
 
 if __name__ == "__main__":
     print("Recupero libri da OpenLibrary...")
